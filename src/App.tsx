b54bc808308fd39c39aec99ba985105325d8d961
Axel Roland-Gosselin <rg_axel@yahoo.fr>
2022-07-10 22:13:22 +0200

    make the poc working

--- src/App.tsx
+++ src/App.tsx
@@ -7,52 +7,104 @@ function App() {
   const [transitionGroupElements, setTransitionGroupElements] = useState(1);
 
   return (
-    <div className="App">
-      <div>
-        <div>Tansition</div>
-        <XyzTransition mode="out-in">
-          <div className="App-anim-placeholder">
-            <button onClick={() => setShowTransition(!showTransition)}>
-              toggled
-            </button>
-            const toggled = false
-            <XyzTransition appear xyz="fade rotate-right ease-out-back">
-              {showTransition && <div className="square" />}
+    <div className="App container">
+      <div className="row">
+        <div className="col">
+          <label className="form-label border b-0">Tansition</label>
+          <div>
+            <div className="form-check form-switch">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                checked={showTransition}
+                onClick={() => setShowTransition(!showTransition)}
+              />
+              <label className="form-check-label">Display</label>
+            </div>
+            <XyzTransition appear xyz="fade">
+              {showTransition && (
+                <div className="container">
+                  <XyzTransitionGroup
+                    className="row"
+                    appear
+                    xyz="fade stagger small"
+                  >
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                  </XyzTransitionGroup>
+                  <XyzTransitionGroup
+                    className="row"
+                    appear
+                    xyz="fade stagger small"
+                  >
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                  </XyzTransitionGroup>
+                  <XyzTransitionGroup
+                    className="row"
+                    appear
+                    xyz="fade stagger small"
+                  >
+                    <div className="col square border bg-primary" />
+                    <div className="col square border bg-primary" />
+                  </XyzTransitionGroup>
+                  <XyzTransitionGroup
+                    className="row"
+                    appear
+                    xyz="fade stagger small"
+                  >
+                    <div className="col square border bg-primary" />
+                  </XyzTransitionGroup>
+                </div>
+              )}
             </XyzTransition>
           </div>
-        </XyzTransition>
+        </div>
       </div>
-      <div>
-        <div>TransitionGroup</div>
-        <button
-          onClick={() =>
-            setTransitionGroupElements(transitionGroupElements + 1)
-          }
-        >
-          Add
-        </button>
-        const toggled = false
-        <button
-          onClick={() => {
-            if (transitionGroupElements <= 0) {
-              setTransitionGroupElements(0);
-            } else {
-              setTransitionGroupElements(transitionGroupElements - 1);
-            }
-          }}
-        >
-          Remove
-        </button>
-        const toggled = false
-        <XyzTransitionGroup
-          appear
-          className="example-grid"
-          xyz="fade small out-down out-rotate-right appear-stagger"
-        >
-          {[...Array(transitionGroupElements)].map((_, index) => (
-            <div className="square" key={index} />
-          ))}
-        </XyzTransitionGroup>
+      <div className="row mt-4">
+        <div className="col">
+          <label className="form-label border">TransitionGroup</label>
+          <div>
+            <button
+              className="btn btn-secondary border"
+              onClick={() =>
+                setTransitionGroupElements(transitionGroupElements + 1)
+              }
+            >
+              Add
+            </button>
+            <button
+              className="btn btn-secondary border"
+              onClick={() => {
+                if (transitionGroupElements <= 0) {
+                  setTransitionGroupElements(0);
+                } else {
+                  setTransitionGroupElements(transitionGroupElements - 1);
+                }
+              }}
+            >
+              Remove
+            </button>
+            <div className="container">
+              <XyzTransitionGroup
+                className="row"
+                appear
+                xyz="fade small out-down out-rotate-right appear-stagger"
+              >
+                {[...Array(transitionGroupElements)].map((_, index) => (
+                  <div
+                    className="square col border bg-primary"
+                    key={index}
+                  ></div>
+                ))}
+              </XyzTransitionGroup>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
